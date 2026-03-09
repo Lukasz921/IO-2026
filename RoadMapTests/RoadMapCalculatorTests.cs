be6e4d260a5f36e171c8cc916e4729f1b3cd1283
Łukasz Przybylski <01189155@pw.edu.pl>
2026-03-09 12:18:19 +0100

    stage_3_red

--- RoadMapTests/RoadMapCalculatorTests.cs
+++ RoadMapTests/RoadMapCalculatorTests.cs
@@ -5,30 +5,38 @@ namespace RoadMapTests
     public class RoadMapCalculatorTests
     {
         [Fact]
-        public void Calculate_Standard_ReturnsCorrectCost()
+        public void Calculate_Len_ReturnsCorrectLen()
         {
             RoadMapCalculator roadmapcalculator = new();
             RoadMap roadmap = new();
-            var result = roadmapcalculator.Calculate("Warszawa", "Kraków", roadmap);
+            var result = roadmapcalculator.Calculate_Length("Warszawa", "Kraków", roadmap);
             Assert.Equal(300, result);
         }
         [Fact]
-        public void Calculate_Standard_ThrowsException()
+        public void Calculate_Len_SameCities_ThrowsException()
         {
             RoadMapCalculator roadmapcalculator = new();
             RoadMap roadmap = new();
             var ex = Assert.Throws<ArgumentException>(() => 
-                roadmapcalculator.Calculate("Warszawa", "Warszawa", roadmap));
+                roadmapcalculator.Calculate_Length("Warszawa", "Warszawa", roadmap));
             Assert.Equal("Cities cannot be the same.", ex.Message);
         }
         [Fact]
-        public void Calculate_NonExistingCity_ThrowsException()
+        public void Calculate_Len_NonExistingCity_ThrowsException()
         {
             RoadMapCalculator roadmapcalculator = new();
             RoadMap roadmap = new();
             var ex = Assert.Throws<ArgumentException>(() =>
-                roadmapcalculator.Calculate("Warszawa", "Andrzej", roadmap));
+                roadmapcalculator.Calculate_Length("Warszawa", "Andrzej", roadmap));
             Assert.Equal("There is no such connection in roadmap.", ex.Message);
+        }
+        [Fact] 
+        public void Calculate_Cost_ReturnsCorrectCost()
+        {
+            RoadMapCalculator roadmapcalculator = new();
+            RoadMap roadmap = new();
+            var result = roadmapcalculator.Calculate_Cost("Warszawa", "Kraków", roadmap, 2);
+            Assert.Equal(600, result);
         }
     }
 }
--- io_1/Elements/RoadMapCalculator.cs
+++ io_1/Elements/RoadMapCalculator.cs
@@ -2,7 +2,7 @@
 {
     public class RoadMapCalculator
     {
-        public int Calculate(string city_A, string city_B, RoadMap roadmap)
+        public int Calculate_Length(string city_A, string city_B, RoadMap roadmap)
         {
             if (city_A == city_B)
             {
@@ -17,6 +17,12 @@
                 return (int)roadmap.GetRoadMap[(city_B, city_A)];
             }
             throw new ArgumentException("There is no such connection in roadmap.");
+        }
+        public int Calculate_Cost(string city_A, string city_B, RoadMap roadmap, int rate_per_km)
+        {
+            //int len = Calculate_Length(city_A, city_B, roadmap);
+            //return len * rate_per_km;
+            throw new NotImplementedException();
         }
     }
 }
